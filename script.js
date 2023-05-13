77630f738c5bbbda2fe55ef266bd881cb152b621
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-05-13 12:12:04 +0530

    Updated script.js for F2 contest 1

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(element,index,list) {
+    if(element.marks>50){
+      console.log(element);
+    }
+  }) 
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(element,index,list) {
+      if(element.marks>50){
+        console.log(element);
+      }
+    })
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45});
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let output = arr.filter(function(element,index,list){
+    return (element.marks>50);
+  })
+  console.log(output);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 4, name: "kishan", age: '22', marks: 95 },
+    { id: 5, name: "gayle", age: '42', marks: 80 },
+    { id: 6, name: "virat", age: '34', marks: 90 },
+  ];
+
+  let concatArr = arr.concat(arr2);
+  console.log(concatArr);
 }
